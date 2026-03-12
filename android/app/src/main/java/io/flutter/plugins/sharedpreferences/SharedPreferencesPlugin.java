76184123deb6a6278db911efaccf7734a4b14521
Jeremy Goldberg <goldberg.jeremy02@gmail.com>
2026-03-12 14:15:22 -0400

    Add shared preferences Android plugin shim

---
+++ android/app/src/main/java/io/flutter/plugins/sharedpreferences/SharedPreferencesPlugin.java
@@ -0,0 +1,7 @@
+package io.flutter.plugins.sharedpreferences;
+
+/**
+ * Temporary shim for builds where the packaged shared_preferences Android
+ * artifact exposes only the legacy plugin class.
+ */
+public final class SharedPreferencesPlugin extends LegacySharedPreferencesPlugin {}
